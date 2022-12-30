e767a7fb107b70c81c9bfd36c5b4d97ecb9b56cd
Gizem <gizemustahuseyin123@gmail.com>
2022-12-30 19:18:21 +0300

    Add files via upload

---
+++ WhileDowhile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDowhileLoops
+{
+    internal class WhileDowhile
+    {
+        static void Main(string[] args)
+        {
+            //while loops
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is {0}", number);
+            Console.ReadKey();
+
+            //DoWhile loops
+            int number1 = 200;
+            do
+            {
+                Console.WriteLine(number1);
+                number1--;
+            } while (number1 >= 100);
+            Console.WriteLine("Now number is {0}", number1);
+            Console.ReadKey();
+        }
+
+    }
+}
+
